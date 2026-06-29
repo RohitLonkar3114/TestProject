48db2c33b1a1dbef3b4c5aece86d984c44323068
RohitLonkar3114 <rohitlonkar3114@gmail.com>
2026-06-29 16:25:32 +0530

    Add Addition Function

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -1,9 +1,17 @@
 
 public class Test {
+	public void Addition()
+	{
+		int a=20;
+		int b=10;
+		int c;
+		c=a+b;
+		System.out.println("Addition = "+c);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Test t1=new Test();
+		t1.Addition();
 	}
 
 }
